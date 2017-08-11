5fedfd844bfad48b909a0ffeb8c86850958aff48
linmingxiong <272921246@qq.com>
2017-08-11 13:32:35 +0800

    添加登录记住我的功能

--- js/login.js
+++ js/login.js
@@ -32,14 +32,38 @@
 				minimum: 6,
 				message: "密码不足6位数"
 			}
-		}
+		},
+		remember: {}
 	};
+	
+  var formValidate = {
+    username: localStorage.getItem('remember:username') || '',
+    password: localStorage.getItem('remember:password') || '',
+    remember: localStorage.getItem('remember') ? true : false
+  }
+  mui('#username')[0].value = formValidate.username
+  mui('#password')[0].value = formValidate.password
+  mui('#remember')[0].checked = formValidate.remember
+  
 	mui(".login").on('tap', '#submit', function() {
 		$valids(constraints, function(attributes) {
 			window.backdrop('正在登录')
+			attributes.remember = mui('#remember')[0].checked
+			formValidate = attributes
 			$http('app/login/login', attributes, function(req) {
 				if(req.res_code === 200) {
 					setState(req.res_data)
+					
+					if (attributes.remember) {
+            localStorage.setItem('remember:username', attributes.username)
+            localStorage.setItem('remember:password', attributes.password)
+            localStorage.setItem('remember', 1)
+          } else {
+            localStorage.removeItem('remember:username')
+            localStorage.removeItem('remember:password')
+            localStorage.removeItem('remember')
+          }
+          
 					swap('index')
 				} else {
 					mui.toast(req.res_data ? req.res_data : '登录失败')
