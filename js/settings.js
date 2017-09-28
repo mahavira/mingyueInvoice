7b193161568dbb16d693bd06783a7bcece171154
linmingxiong <272921246@qq.com>
2017-09-28 19:53:14 +0800

    ~添加纳税人识别号

--- js/settings.js
+++ js/settings.js
@@ -18,6 +18,7 @@
 		mui('#company_id')[0].innerText = user.office.idCode || ''
 		mui('#company_name')[0].innerText = user.office.name || ''
 		mui('#mobile')[0].innerText = user.mobile || ''
+		mui('#taxpayerIdNum')[0].innerText = user.taxpayerIdNum || ''
 	}
 
 	var urls = {
